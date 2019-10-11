e0f581ead3a8c72b79d0219198dd97c52427349c
Gino Villalpando <grieveous@live.com>
2019-10-11 09:06:09 -0600

    the snap challenge - use functions to add 2 strings

---
+++ snap-10-10-2019.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<title>User Story for mailchimp</title>
+</head>
+	<body>
+<p>"As a power user, I can connect using my facebook that's also connected to my instagram
+	business profile to access photos from instagram for Content Studio" - <em><strong>steve p.</strong></em></p>
+	<img src="snapchallenge-ERDDiagram.png" alt="Entity Relationship Diagram" title="My ERD for my user's story">
+	</body>
+</html>
