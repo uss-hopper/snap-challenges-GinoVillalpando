f0d3a8821d82bad81146bb39abd82b63251f6fad
Gino Villalpando <grieveous@live.com>
2019-10-21 09:10:59 -0600

    wrote a class with two state variables, accessors, mutators, started writing constructor

---
+++ snap-10-21-2019.php
@@ -0,0 +1,89 @@
+<?php
+
+
+
+
+class profile {
+	/**
+	 * id for this profile; this is the primary key
+	 * @var int $profileId
+	 */
+	private $profileId;
+
+	/**
+	 * profile name for this profile
+	 * @var string $profileName
+	 */
+	private $profileName;
+
+	/**
+	 * constructor for this profile
+	 * @param int $newProfileId id of this profile or null if a new profile
+	 * @param string
+	 */
+	public function __construct($newProfileId, ?string $newProfileName) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileName($newProfileName);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			//determine what exception type was thrown
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for profile id
+	 */
+		public function getProfileId(): int {
+		return ($this->profileId);
+	}
+
+	/**
+	 * mutator for profile id
+	 */
+
+	public function setProfileId($newProfileId): int {
+		$newProfileId = filter_var($newProfileId, FILTER_VALIDATE_INT);
+		if ($newProfileId === false) {
+			throw (new \http\Exception\UnexpectedValueException("profile id is not a valid integer"));
+	}
+		/**
+		 * convert and store the profile Id
+		 */
+		$this->profileId = intval($newProfileId);
+	}
+
+	/**
+	 * accessor method for profile Name
+	 */
+
+	public function getProfileName(): ?string {
+		return ($this->profileName);
+	}
+
+	/**
+	 * mutator method for profile Name
+	 */
+
+	public function setProfileName(?string $newProfileName): void {
+		if($newProfileName === null) {
+			$this->profileName = null;
+			return;
+		}
+		$newProfileName = strtolower(trim($newProfileName));
+		if(ctype_xdigit($newProfileName) === false) {
+			throw (new \RangeException("This profile name is not valid."));
+		}
+		if(strlen($newProfileName) !== 32) {
+			throw (new \RangeException("Profile Name must be at least 32 Characters."));
+		}
+		$this->profileName = $newProfileName;
+	}
+
+		/**
+		 * create an HTML formatted profile
+		 */
+		$gino = new profile (1, "grievxus");
+
+}
