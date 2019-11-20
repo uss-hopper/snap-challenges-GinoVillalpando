9bbf249d884f75fed49d89b79f651314c2a51f7c
Gino Villalpando <grieveous@live.com>
2019-11-20 10:32:53 -0700

    added answer to question 1 to snap challenge

--- snap-11-20-2019/snap-11-20-2019.js
+++ snap-11-20-2019/snap-11-20-2019.js
@@ -1,17 +1,33 @@
-1.) input: arr [] = {1, 1, 2, 2, 2, 2, 3}
-let theNumber = 2;
-return arr;
+input: $origArray = [1, 1, 2, 2, 2, 2, 3]
+echo(counts($origArray));
 
-output: 4 // x (or 2) occurs 4 times in arr[]
 
-2.) for (x = 3 or % 2 == 0, arr) do {
-	if (arr % 2 == 0) then print "fizz";
-	else if (arr is divisble by 3) then print "buzz" {
-		else print arr;
-	}
-	else (arr is divisable by 3 == arr % 2 == 0) then print "baz"
+function counts ($array){
+
+	$newArray = [];
+
+	foreach( $array as $element){
+		if (isset($newArray[$element])){
+			$newArray[$element] = $newArray[$element] + 1;
+		}else{
+			$newArray[$element] = 1;
+		};
+
+		echo($element . " " . $newArray[$element] . PHP_EOL);
+	};
+	return $newArray;
 };
 
-return arr;
 
-output: "fizz"
+output: 4 // x (or 2) occurs 4 times in arr
+
+for ($origArray = % 3 or % 2 == 0, $newArray) do {
+	if ($newArray % 2 == 0) then print "fizz";
+	else if ($newArray is divisble by 3) then print "buzz" {
+		else print arr;
+	}
+	else ($newArray is divisable by 3 && $newArray % 2 == 0) then print "baz"
+};
+
+
+output: results
