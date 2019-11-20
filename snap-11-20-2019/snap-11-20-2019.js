89562f037aa24401449825b04749b6333005bb97
Gino Villalpando <grieveous@live.com>
2019-11-20 08:59:41 -0700

    write in psuedocode to find divisors and draw image of example div

---
+++ snap-11-20-2019/snap-11-20-2019.js
@@ -0,0 +1,17 @@
+1.) input: arr [] = {1, 1, 2, 2, 2, 2, 3}
+let theNumber = 2;
+return arr;
+
+output: 4 // x (or 2) occurs 4 times in arr[]
+
+2.) for (x = 3 or % 2 == 0, arr) do {
+	if (arr % 2 == 0) then print "fizz";
+	else if (arr is divisble by 3) then print "buzz" {
+		else print arr;
+	}
+	else (arr is divisable by 3 == arr % 2 == 0) then print "baz"
+};
+
+return arr;
+
+output: "fizz"
