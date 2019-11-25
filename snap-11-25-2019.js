6fc8cd7f9d647a4268aba39ea4d75345445780d9
Gino Villalpando <grieveous@live.com>
2019-11-25 08:35:43 -0700

    wrote functions for deleting vowels, checking for the greatest number in an array and a function that uses the fibonacci sequence to 100

---
+++ snap-11-25-2019.js
@@ -0,0 +1,22 @@
+let string be defined
+
+function deleteVowels(string) {
+	iterate through string using a for loop
+	spit any vowels from the string then replace with empty character
+	return the string
+	}
+
+	let array be defined
+
+	create function that uses math.max to check the array for the greatest number
+
+
+fucntion fibonacciLoop(num) {
+	let a equal 1, b equal 0, then temp number to be stored;
+	while loop that iterates the number is greater than or equal to 0
+	temp equals a;
+	a = a + b;
+	b = temp;
+	decrement the num variable;
+	return b;
+}
