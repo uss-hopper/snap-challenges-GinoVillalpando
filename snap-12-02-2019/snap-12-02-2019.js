b02907a8e9b9b0a9b96b3f6d0e191bea97bd9dc8
Gino Villalpando <grieveous@live.com>
2019-12-02 08:45:33 -0700

    added bootstrap and made list into nav buttons

--- snap-12-02-2019/snap-12-02-2019.js
+++ snap-12-02-2019/snap-12-02-2019.js
@@ -2,7 +2,7 @@ let peopleNames = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah'];
 
 let people = document.querySelector('#people');
 
-people.innerHTML = '<ul>' +
+people.innerHTML = '<ul class="pl-0 my-5 text-center">' +
 	peopleNames.map(function(peopleNames) {
-		return '<li>' + peopleNames + '</li>';
+		return '<li class="btn btn-dark btn-lg col-6 m-1 p-5">' + peopleNames + '</li>';
 	}).join('') + '</ul>';
