04b6d261e360dad05600e90cd5d7a0cd1992dead
Gino Villalpando <grieveous@live.com>
2019-12-05 10:01:41 -0700

    playing around with listing the breeds differently

--- snap-12-05-2019/script.js
+++ snap-12-05-2019/script.js
@@ -4,9 +4,9 @@ function onPageLoad() {
 	axios.get('https://dog.ceo/api/breeds/list/all')
 		.then(({data}) => {
 			let dogs = data.message;
-			let list = '<ul> ';
+			let list = ' ';
 			for (let breed in dogs) {
-				list = list + '<li>' + breed + '</li>';
+				list = list + '<ul><li>' + breed + '</li></ul><hr/>';
 			}
 			let html = document.getElementById("target");
 			html.innerHTML = list;
